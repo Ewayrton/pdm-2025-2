85232b49b61660c9fb47ac2f727b9f9a16abd771
Ewayrton Oliveira <164266609+Ewayrton@users.noreply.github.com>
2025-08-15 23:01:15 +0000

    primeiro commit

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View, Image } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image source={require('./images/foto perfil.jpg')}
+             style={{width: 200, height: 200}}
+             
+      >
+      </Image>
+    </View>
+    
+  );
+}
