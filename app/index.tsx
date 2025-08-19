914013f2ae1c7dd631a23a8027d19e2455118e2e
Ewayrton Brito de Oliveira <ewayrtonbrito@gmail.com>
2025-08-19 18:11:03 -0300

    alterando foto p redondo, add nome e mini bio

--- app/index.tsx
+++ app/index.tsx
@@ -7,15 +7,21 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 20
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Image source={require('./images/foto perfil.jpg')}
-             style={{width: 200, height: 200}}
-             
-      >
-      </Image>
+      <Text style={{fontWeight: "bold"}}>
+        Ewayrton Oliveira.
+      </Text>
+        <Image source={require('./images/foto perfil.jpg')}
+             style={{width: 200, height: 200, borderRadius: 100}}      
+        >
+        </Image>
+          <Text style={{textAlign: "center"}}>
+            Sou apaixonado por tecnologia e desenvolvimento web, atualmente cursando Sistemas para Internet na UNICAP. 
+            Tenho experiência prática com Java, Spring Boot e Next.js, além de já ter desenvolvido projetos como aplicativos de agendamento 
+            e plataformas de gestão. Estou sempre em busca de novos aprendizados e oportunidades que me permitam evoluir como desenvolvedor.
+          </Text>
     </View>
-    
   );
 }
